1919f111e571f1ab06536693d8736984eb55ac8c
reydon227 <dhruvarora@delhinerds.com>
2020-04-08 22:32:59 -0400

    added  csrf exemption

--- eurekaBackend/urls.py
+++ eurekaBackend/urls.py
@@ -5,7 +5,6 @@ from graphene_django.views import GraphQLView
 from .schema import schema
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
-
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('admin/', admin.site.urls),    
 ]
