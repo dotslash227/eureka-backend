fa7a4d6aa6c1825a0f751fb7093112330c3bafb4
reydon227 <dhruvarora@delhinerds.com>
2020-04-22 10:43:25 -0400

    quizzes query for home

--- quiz/schema.py
+++ quiz/schema.py
@@ -24,20 +24,43 @@ class Query(object):
     quiz_by_clubs = graphene.List(QuizType, clubId=graphene.Int(required=True))
     questions_by_quiz = graphene.List(QuestionType, quizId=graphene.Int(required=True))
     options_by_question = graphene.List(OptionType, questionId=graphene.Int(required=True))
+    new_quizes_by_user = graphene.List(QuizType, userId=graphene.Int(required=True))
 
     def resolve_quiz_by_clubs(self, info, **kwargs):
+        # Method to resolve quiz by clubs query
         club_id = kwargs.get("clubId")        
         club = Club.objects.get(pk=club_id)        
         return Quiz.objects.filter(club=club)
 
     def resolve_questions_by_quiz(self, info, **kwargs):
+        # method to resolve questions in a quiz
         quiz_id = kwargs.get("quizId")
         quiz = Club.objects.get(pk=quiz_id)
         return Question.objects.filter(quiz=quiz)
 
     def resolve_options_by_question(self, info, **kwargs):
+        # method to resolve  options of a quiz
         question_id = kwargs.get("questionId")
         question = Question.objects.get(pk=question_id)
         return Option.objects.filter(question=question)
+
+    def resolve_new_quizes_by_user(self, info, **kwargs):
+        # method to resolve upcoming quizzes to be displayed on the main homepage
+        user_id = kwargs.get("userId")
+        user = User.objects.get(pk=user_id)
+        try:
+            user_clubs = Club.objects.filter(members=user)            
+        except:
+            raise Exception("User not part of clubs")
+        else:
+            quizes = []
+            for counter,club in enumerate(user_clubs):
+                if counter == 0:                    
+                    quizes = Quiz.objects.filter(club=club)
+                else:                    
+                    query = Quiz.objects.filter(club=club)
+                    quizes = quizes | query                    
+        return quizes
+
 
 # End of Queries for quizzes
